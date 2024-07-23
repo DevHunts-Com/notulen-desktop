79180533be30b189746afd059b2aeb61b481e26e
ibnumalik99 <ibnumalik69384@gmail.com>
2024-07-23 15:33:07 +0700

    checkpoint

--- electron/main.ts
+++ electron/main.ts
@@ -1,69 +1,84 @@
-import { app, BrowserWindow } from 'electron'
-import { createRequire } from 'node:module'
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { Notulen } from '@tarikhagustia/notulen';
+import { MeetingResult } from '@tarikhagustia/notulen/dist/interfaces';
 
-const require = createRequire(import.meta.url)
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// The built directory structure
-//
-// ├─┬─┬ dist
-// │ │ └── index.html
-// │ │
-// │ ├─┬ dist-electron
-// │ │ ├── main.js
-// │ │ └── preload.mjs
-// │
-process.env.APP_ROOT = path.join(__dirname, '..')
+process.env.APP_ROOT = path.join(__dirname, '..');
 
-// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
-export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
+export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
+export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron');
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist');
 
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST;
 
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 
 function createWindow() {
   win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
+      contextIsolation: true,
+      // enableRemoteModule: false,
+      nodeIntegration: false
     },
-  })
-
-  // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+  });
 
   if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
+    win.loadURL(VITE_DEV_SERVER_URL);
   } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(RENDERER_DIST, 'index.html'))
+    win.loadFile(path.join(RENDERER_DIST, 'index.html'));
   }
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+app.whenReady().then(() => {
+  createWindow();
+
+  ipcMain.handle('start-notulen', async (event, options) => {
+    console.log(event)
+    console.log('Received options:', options);
+    try {
+      const client = new Notulen({
+        debug: false, // debug mode to show the browser
+        name: options.name, // Bot name
+        googleMeetUrl: options.googleMeetUrl, // your google meet link
+        language: "id-ID", // language for caption generation
+        geminiApiKey: options.geminiApiKey, // google gemini api key (get it for free)
+        recordingLocation: options.recordingLocation, // location to save the recording
+        prompt: options.prompt
+      });
+    
+        // Start join the meeting
+      client.listen();
+    
+      client.on("end", (result: MeetingResult) => {
+        // const result = {};
+        // exit process when done
+        process.exit(0);
+        return result;
+      });
+
+      const result = {};
+      return result;
+    } catch (error) {
+      console.error('Error in start-notulen:', error);
+      throw error;
+    }
+  });
+});
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
+    app.quit();
+    win = null;
   }
-})
+});
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
-
-app.whenReady().then(createWindow)
-app.disableHardwareAcceleration();
+});
--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,35 +1,105 @@
-import '../App.css';
-import 'flowbite';
+import React, { useState } from 'react';
 
-function App() {
-
-  return (
-    <>
-      <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="p-4 w-full max-w-xl">
-              <div className="bg-white rounded-lg shadow">
-                  <div className="p-4 md:p-5">
-                      <form className="space-y-4">
-                          <div>
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder='My Assistant' required />
-                          </div>
-                          <div>
-                            <label htmlFor="meet_url">Url Meet</label>
-                            <input type="text" name="meet_url" id="meet_url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder='https://meet.google.com/xxx-xxx-xxx' required />
-                          </div>
-                          <div>
-                            <label htmlFor="gemini_api_key">Gemini Api Key</label>
-                            <input type="text" name="gemini_api_key" id="gemini_api_key" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder='••••••••' required />
-                          </div>
-                          <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Start</button>
-                      </form>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </>
-  )
+interface InputValue {
+  name: string;
+  meet_url: string;
+  gemini_api_key: string;
 }
 
-export default App
+interface MeetingResult {
+  summary: string;
+}
+
+function App() {
+  const [inputValue, setInputValue] = useState<InputValue>({
+    name: '',
+    meet_url: '',
+    gemini_api_key: '',
+  });
+  const [result, setResult] = useState<MeetingResult | null>(null);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const key = e.target.id as keyof InputValue;
+    const value = e.target.value;
+    setInputValue((prev) => ({ ...prev, [key]: value }));
+  }
+
+  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // try {
+      const result = await window.ipcRenderer.invoke('start-notulen', {
+        debug: true,
+        name: inputValue.name,
+        googleMeetUrl: 'https://meet.google.com/tnx-atdh-yat',//inputValue.meet_url,
+        language: 'id-ID',
+        geminiApiKey: 'AIzaSyC6jfxBUCTV6LBcxYUdPnHU0gfQI81oXV8',// inputValue.gemini_api_key,
+        recordingLocation: './out',
+        prompt: 'Kamu adalah seorang Asisten Note Takker...',
+      });
+      setResult(result);
+      console.log(result)
+    // } catch (error) {
+    //   console.error('Error:', error);
+    // }
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="p-4 w-full max-w-xl">
+        <div className="bg-white rounded-lg shadow">
+          <div className="p-4 md:p-5">
+            <form className="space-y-4" onSubmit={handleOnSubmit}>
+              <div>
+                <label htmlFor="name">Name</label>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                  placeholder="My Assistant"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="meet_url">Url Meet</label>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  id="meet_url"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                  placeholder="https://meet.google.com/xxx-xxx-xxx"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="gemini_api_key">Gemini Api Key</label>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  id="gemini_api_key"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                  placeholder="••••••••"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-5 py-2.5"
+              >
+                Start
+              </button>
+            </form>
+            {result && (
+              <div>
+                <h3>Summary:</h3>
+                <p>{result.summary}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
